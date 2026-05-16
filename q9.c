756be9f2861c966b71ae5db616a6f63d3034c219
Tainá Barbosa <160445609+Nabarbosa@users.noreply.github.com>
2026-05-16 21:24:42 +0000

    feito

--- q9.c
+++ q9.c
@@ -1,23 +1,78 @@
 #include <stdio.h>
 
-#define QTD_NAVIOS 2
+#define QTD_NAVIOS 5
 #define TAM_TAB 10
 
 void mostrarTab(char tab[TAM_TAB][TAM_TAB]);
 void iniciarTab(char tab[TAM_TAB][TAM_TAB]);
 int posicaoNavioValida(int l, int c);
 void inserirNavios(char tabJogador[TAM_TAB][TAM_TAB]);
-void transformarEmChar(int linha, int coluna);
+void transformarEmChar(int *linha, int *coluna, char jogada[]);
+int navioAbatido(char tabJogador[TAM_TAB][TAM_TAB], int jogador);
+void mostrarTabOculto(char tab[TAM_TAB][TAM_TAB]);
 
 int main(){
     char tabJogador1[TAM_TAB][TAM_TAB];
+    char tabJogador2[TAM_TAB][TAM_TAB];
+    int jogador1 = 1;
+    int jogador2 = 2;
+
+    int jogadorAtual = 1;
+    int venceu = 0;
 
     iniciarTab(tabJogador1);
-    mostrarTab(tabJogador1);    
+    iniciarTab(tabJogador2);
 
+    printf("Jogador 1 posiciona os navios\n");
+    mostrarTab(tabJogador1);
     inserirNavios(tabJogador1);
 
-    mostrarTab(tabJogador1);
+    printf("Jogador 2 posiciona os navios\n");
+    mostrarTab(tabJogador2);
+    inserirNavios(tabJogador2);
+
+    while(!venceu){
+
+        switch(jogadorAtual){
+
+            case 1:
+
+                printf("\n=== VEZ DO JOGADOR 1 ===\n");
+
+                printf("\nSeu tabuleiro:\n");
+                mostrarTab(tabJogador1);
+
+                printf("\nTabuleiro inimigo:\n");
+                mostrarTabOculto(tabJogador2);
+
+                venceu = navioAbatido(tabJogador2, 1);
+
+                jogadorAtual = 2;
+
+                break;
+
+
+            case 2:
+
+                printf("\n=== VEZ DO JOGADOR 2 ===\n");
+
+                printf("\nSeu tabuleiro:\n");
+                mostrarTab(tabJogador2);
+
+                printf("\nTabuleiro inimigo:\n");
+                mostrarTabOculto(tabJogador1);
+
+                venceu = navioAbatido(tabJogador1, 2);
+
+                jogadorAtual = 1;
+
+                break;
+        }
+    }
+
+    printf("Jogador %d venceu!\n", jogadorAtual == 1 ? 2 : 1);
+
+    return 0;
 }
 
 void iniciarTab(char tab[TAM_TAB][TAM_TAB]){
@@ -60,16 +115,19 @@ void inserirNavios(char tabJogador[TAM_TAB][TAM_TAB]){
     int totalNavios = 0;
     char jogada[TAM_TAB];
     int direcao = 0;
+    int qtdTotalNavios = 0;
 
     while(totalNavios < QTD_NAVIOS){
         printf("Informe o tamanho do navio: 1 - 2 - 3 - 4\n");
         scanf("%d", &tam_navio);
 
+        qtdTotalNavios = tam_navio;
+
         if(tam_navio == 1){
             printf("Informe a posição do seu navio, jogador\n");
             scanf("%s", jogada);
 
-            transformarEmChar(linha, coluna);
+            transformarEmChar(&linha, &coluna, jogada);
 
             if(!posicaoNavioValida(linha, coluna)){
                 printf("Jogada Inválida!!");
@@ -92,7 +150,7 @@ void inserirNavios(char tabJogador[TAM_TAB][TAM_TAB]){
                 printf("Informe a posição do seu navio, jogador\n");
                 scanf("%s", jogada);
 
-                transformarEmChar(linha, coluna);
+                transformarEmChar(&linha, &coluna, jogada);
 
                 if(linha - tam_navio + 1 < 0){
                     printf("Navio maior que o limite!\n");
@@ -104,7 +162,7 @@ void inserirNavios(char tabJogador[TAM_TAB][TAM_TAB]){
                     continue;
                 }
                 
-                for(int k = linha + tam_navio; k > linha; k--){
+                for(int k = linha; k > linha - tam_navio; k--){
                     tabJogador[k][coluna] = 'N';
                 }
 
@@ -118,7 +176,7 @@ void inserirNavios(char tabJogador[TAM_TAB][TAM_TAB]){
                 printf("Informe a posição do seu navio, jogador\n");
                 scanf("%s", jogada);
 
-                transformarEmChar(linha, coluna);
+                transformarEmChar(&linha, &coluna, jogada);
 
                 if(linha + tam_navio > TAM_TAB){
                     printf("Navio maior que o limite!\n");
@@ -144,7 +202,7 @@ void inserirNavios(char tabJogador[TAM_TAB][TAM_TAB]){
                 printf("Informe a posição do seu navio, jogador\n");
                 scanf("%s", jogada);
 
-                transformarEmChar(linha, coluna);
+                transformarEmChar(&linha, &coluna, jogada);
 
                 if(coluna + tam_navio > TAM_TAB){
                     printf("Navio maior que o limite!\n");
@@ -170,7 +228,7 @@ void inserirNavios(char tabJogador[TAM_TAB][TAM_TAB]){
                 printf("Informe a posição do seu navio, jogador\n");
                 scanf("%s", jogada);
 
-                transformarEmChar(linha, coluna);
+                transformarEmChar(&linha, &coluna, jogada);
 
                 if(coluna - tam_navio + 1 < 0){
                     printf("Navio maior que o limite!\n");
@@ -182,7 +240,7 @@ void inserirNavios(char tabJogador[TAM_TAB][TAM_TAB]){
                     continue;
                 }
 
-                for(int k = coluna + tam_navio; k > coluna; k--){
+                for(int k = coluna; k > coluna - tam_navio; k--){
                     tabJogador[linha][k] = 'N';
                 }
 
@@ -196,42 +254,55 @@ void inserirNavios(char tabJogador[TAM_TAB][TAM_TAB]){
     }
 }
 
-void navioAbatido(char tabJogador[TAM_TAB][TAM_TAB], char navio){
-    char jogada[TAM_TAB];
-    int tiros = TAM_TAB * TAM_TAB;
+int navioAbatido(char tabJogador[TAM_TAB][TAM_TAB], int jogador){
+    char jogada[10];
     int linha, coluna;
-    int i = 0;
+    int naviosRestantes = 0;
 
-    while(jogada < tiros * 2 || jogada < QTD_NAVIOS){
-        printf("Informe em que posição deseja atirar: \n");
-        scanf("%s", jogada);
-
-        transformarEmChar(linha, coluna);
-
-        if(!posicaoNavioValida(linha, coluna)){
-            printf("Jogada Inválida!!");
-        }
-
-        if(jogada == 'N'){
-            tabJogador[linha][coluna] = 'O';
-
-            while(jogada == 'N'){
-                printf("Informe em que posição deseja atirar: \n");
-                scanf("%s", jogada);
-
-                transformarEmChar(linha, coluna);
-
-                if(!posicaoNavioValida(linha, coluna)){
-                    printf("Jogada Inválida!!");
-                }
-
-                tabJogador[linha][coluna] = 'O';
-            }
-
-        } else {
-            tabJogador[linha][coluna] = 'X';
+    for(int i = 0; i < TAM_TAB; i++){
+        for(int j = 0; j < TAM_TAB; j++){
+            if(tabJogador[i][j] == 'N'){
+                naviosRestantes++;
+            }    
         }
     }
+
+    printf("\nJogador %d atira!\n", jogador);
+
+    while(1){
+        printf("Digite a posição: ");
+        scanf("%s", jogada);
+
+        transformarEmChar(&linha,&coluna,jogada);
+
+        if(!posicaoNavioValida(linha,coluna)){
+            printf("Posição inválida!\n");
+            continue;
+        }
+
+        if(tabJogador[linha][coluna] == 'N'){
+            printf("Acertou!\n");
+            tabJogador[linha][coluna] = 'O';
+            naviosRestantes--;
+
+            if(naviosRestantes == 0){
+                return 1;
+            }
+
+            mostrarTab(tabJogador);
+            continue;
+        }
+
+        if(tabJogador[linha][coluna] == ' '){
+            printf("Errou!\n");
+            tabJogador[linha][coluna] = 'X';
+            break;
+        }
+
+        printf("Posição já jogada!\n");
+    }
+
+    return 0;
 }
 
 int posicaoNavioValida(int l, int c){
@@ -242,13 +313,33 @@ int posicaoNavioValida(int l, int c){
     return 1;
 }
 
-void transformarEmChar(int linha, int coluna){
-    char jogada[TAM_TAB];
-
-    if(jogada[0] >= 'a' && jogada[0] <= 'z'){
+void transformarEmChar(int *linha, int *coluna, char jogada[]){
+    if(jogada[0] >= 'a' && jogada[0] <= 'z')
         jogada[0] -= 32;
-    }
 
-    linha = jogada[0] - 'A';
-    coluna = jogada[1] - '1';
+    *linha = jogada[0] - 'A';
+
+    if(jogada[2] == '\0')
+        *coluna = jogada[1] - '1';
+    else
+        *coluna = 9;
+}
+
+void mostrarTabOculto(char tab[TAM_TAB][TAM_TAB]){
+    printf("\n");
+    printf("\n   1   2   3   4   5   6   7   8   9   10\n");
+
+    for(int i = 0; i < TAM_TAB; i++){
+        printf("%c ", 'A' + i);
+
+        for(int j = 0; j < TAM_TAB; j++){
+
+            if(tab[i][j] == 'N')
+                printf("   |"); 
+            else
+                printf(" %c |", tab[i][j]);
+        }
+
+        printf("\n------------------------------------------\n");
+    }
 }
