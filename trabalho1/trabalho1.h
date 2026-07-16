df77dcc294f19b5470efd99dd580f351d0866442
Tainá Barbosa <160445609+Nabarbosa@users.noreply.github.com>
2026-07-16 13:51:20 +0000

    att

--- trabalho1/trabalho1.h
+++ trabalho1/trabalho1.h
@@ -27,7 +27,8 @@ typedef struct DQ
     int iDia; 
     int iMes;
     int iAno;
-    int valido; // 0 se inválido, e 1 se válido 
+    int valido; 
+    // 0 se inválido, e 1 se válido 
 
 } DataQuebrada;
 
